1b83f2d189f2a3aaec6681111263dca3e00040a8
Hindami M <56010150+hindami@users.noreply.github.com>
2020-02-11 13:15:13 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,64 @@
+var myGamePiece;
+var myGamePieceDua;
+
+function startGame() {
+    myGameArea.start();
+    myGamePiece = new component(8, 20, "red", 45, 290);
+    myGamePiece2 = new component(8, 20, "red", 544, 290);
+}
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 600;
+        this.canvas.height = 600;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+        window.addEventListener('keydown', function (e) {
+            myGameArea.key = e.keyCode;
+        })
+        window.addEventListener('keyup', function (e) {
+            myGameArea.key = false;
+        })
+    },
+    clear : function(){
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
+function component(width, height, color, x, y) {
+    this.gamearea = myGameArea;
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+    this.update = function() {
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+}
+
+
+function updateGameArea() {
+    myGameArea.clear();
+    myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0;
+    myGamePiece2.speedX = 0;
+    myGamePiece2.speedY = 0;
+    if (myGameArea.key && myGameArea.key == 37) {myGamePiece.speedX = -3; myGamePiece2.speedX = -3; }
+    if (myGameArea.key && myGameArea.key == 39) {myGamePiece.speedX = 3; myGamePiece2.speedX = 3; }
+    if (myGameArea.key && myGameArea.key == 38) {myGamePiece.speedY = -3; myGamePiece2.speedY = -3; }
+    if (myGameArea.key && myGameArea.key == 40) {myGamePiece.speedY = 3; myGamePiece2.speedY = 3; }
+    myGamePiece.newPos();
+    myGamePiece.update();
+    myGamePiece2.newPos();
+    myGamePiece2.update();
+}
